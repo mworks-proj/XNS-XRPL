600125f50bb4fb1e3389fb8b1bb5af3de9379d1f
meister <78353166+mworks-proj@users.noreply.github.com>
2023-07-20 14:17:28 -0700

    preparing domain search test

--- xns/src/App.js
+++ xns/src/App.js
@@ -1,38 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './xrpl.png';
-
-import TypewriterSearchBar from './components/searchbar/typewriterSearchBar';
+//import TypewriterSearchBar from './components/searchbar/typewriterSearchBar'; // Importing Custom Placeholder animation
+import DomainSearch from './components/api/DomainSearch'; // Importing Domain Search API 
 
 const App = () => {
-  const [registeredTLDs, setRegisteredTLDs] = useState([]);
-  const [filteredTLDs, setFilteredTLDs] = useState([]);
-
-  const handleSearch = () => {
-    // Simulated logic to fetch registered TLDs based on user input
-    // Replace with your own logic to retrieve TLDs from the backend or external source
-    const simulatedRegisteredTLDs = [
-      'logistics.xrpl',
-      'test.xrpl',
-      'WoJ4ke.xrpl',
-      'EverNode.xrpl',
-      'Gem.xrpl'
-    ];
-  
-    setRegisteredTLDs(simulatedRegisteredTLDs);
-    setFilteredTLDs(simulatedRegisteredTLDs); // Update filteredTLDs with the registered TLDs
-  };
-  
-
-  const handleInputChange = (e) => {
-    const inputValue = e.target.value;
-
-    // Filter registered TLDs based on user input
-    const filteredTLDs = registeredTLDs.filter((tld) => tld.includes(inputValue));
-    setFilteredTLDs(filteredTLDs);
-  };
-
   return (
     <div className="container">
       <header className="header">
@@ -51,23 +24,9 @@ const App = () => {
         </div>
         <div className="search-container">
           <h1 className="title">XRPL Naming Service</h1>
-          <div className="search-bar">
-            {/* Pass the handleSearch function as a prop to TypewriterSearchBar */}
-            <TypewriterSearchBar onSearch={handleSearch} />
-          </div>
-          {/* Removed the resolvedName div as it was not being used */}
+          <DomainSearch filteredTLDs={DomainSearch.filteredTLDs} />
         </div>
-
-        {filteredTLDs.length > 0 && (
-          <div className="tld-results">
-            <h2>Registered TLDs</h2>
-            <ul>
-              {filteredTLDs.map((tld) => (
-                <li key={tld}>{tld}</li>
-              ))}
-            </ul>
-          </div>
-        )}
+        
       </main>
     </div>
   );
---
+++ xns/src/components/api/DomainSearch.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const DomainSearch = () => {
+  const [filteredTLDs, setFilteredTLDs] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSearch = async () => {
+    try {
+      const options = {
+        method: 'POST',
+        url: 'https://domain-suggestion-engine.p.rapidapi.com/api/domain-suggest',
+        headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': '631fb252b0msh8f0bbecc4c8cf7ep11c039jsn4d1489194a87',
+          'X-RapidAPI-Host': 'domain-suggestion-engine.p.rapidapi.com'
+        },
+        data: {
+          query: searchTerm, // Use the domain name entered by the user for the search
+          search_type: [
+            'available',
+            'registered'
+          ],
+          tlds: ['com', 'net'],
+          idn: true,
+          page_size: 25,
+          algorithm: 'full',
+          score: {
+            com: 1,
+            net: 0.9,
+            org: 0.8,
+            pl: 0.7,
+            bg: 0.8
+          },
+          language: 'fr'
+        }
+      };
+
+      const response = await axios(options);
+
+      const data = response.data;
+      if (Array.isArray(data)) {
+        // Extract the domain names from the API response data
+        const domainNames = data.map(item => item.domain);
+        setFilteredTLDs(domainNames); // Update filteredTLDs with the domain names
+        setErrorMessage('');
+      } else {
+        setFilteredTLDs([]);
+        setErrorMessage('No domain names found.');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setFilteredTLDs([]);
+      setErrorMessage('Error fetching data. Please try again later.');
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    setSearchTerm(inputValue);
+  };
+
+  return (
+    <div>
+      <div className="search-bar">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search for domain names"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button className="btn btn-primary" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+      {errorMessage ? (
+        <div className="error-message">{errorMessage}</div>
+      ) : (
+        filteredTLDs.length > 0 && (
+          <div className="tld-results">
+            <h2>Domain Names</h2>
+            <ul>
+              {filteredTLDs.map((domainName) => (
+                <li key={domainName}>{domainName}</li>
+              ))}
+            </ul>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default DomainSearch;
